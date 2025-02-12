7a723d9ae006378c5adf5a694c3180a35a6289ca
josip <josip@laptop>
2025-02-12 17:55:33 +0100

    update da koristi metode

--- src/MinMaxNiz.java
+++ src/MinMaxNiz.java
@@ -3,48 +3,52 @@ import java.util.Scanner;
 
 public class MinMaxNiz {
     public static void main(String[] args) {
-        int[] nizBrojeva = new int[0];
-        Scanner scanner = new Scanner(System.in);
+        int[]nizBrojeva =  ucitajPolje();
+        ispisPolja(nizBrojeva);
+        System.out.println("Najveci broj je " + nadjiMax(nizBrojeva) + " a najmanji " + nadjiMin(nizBrojeva));
+    }
 
-        int uneseniBroj = 0;
-
+    public static int[] ucitajPolje() {
         System.out.println("Koliko zelite brojeva : ");
+        Scanner scanner = new Scanner(System.in);
         int velicinaPolja = scanner.nextInt();
 
-        nizBrojeva = new int[velicinaPolja];
-
+        int[] nizBrojeva = new int[velicinaPolja];
         for (int i = 0; i < velicinaPolja; i++) {
             System.out.println("Unesite broj: ");
-            uneseniBroj = scanner.nextInt();
-            if (uneseniBroj != -1) {
-                nizBrojeva[i]= uneseniBroj;
-            }
+            nizBrojeva[i] = scanner.nextInt();
         }
+        scanner.close();
+        return nizBrojeva;
+    }
 
+    public static void ispisPolja(int[] nizBrojeva) {
         System.out.println("Uneseno polje je ");
-
         System.out.print("[");
-        for (int i : nizBrojeva) {
-            System.out.print(i + ",");
+        for (int i = 0; i < nizBrojeva.length -1; i++) {
+            System.out.print(nizBrojeva[i] + ",");
         }
-        System.out.print("]");
-
+        System.out.print(nizBrojeva[nizBrojeva.length - 1] + "]");
         System.out.println();
+    }
 
-        int min = nizBrojeva[0];
-        int max = nizBrojeva[0];
 
-        for (Integer i : nizBrojeva) {
+    public static int nadjiMin(int[] niz) {
+        int min = niz[0];
+        for (Integer i : niz) {
             if (i < min) {
                 min = i;
             }
+        }
+        return min;
+    }
+    public static int nadjiMax(int[] niz) {
+        int max = niz[0];
+        for (Integer i : niz) {
             if (i > max) {
                 max = i;
             }
         }
-
-        System.out.println("Najveci broj je " + max + " a najmanji " + min);
-        scanner.close();
-
+        return max;
     }
 }
